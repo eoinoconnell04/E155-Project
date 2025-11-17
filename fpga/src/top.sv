ad9cbb3e7066a716ad3bce7ca13bdc51ec11ffa2
Eoin O'Connell <eoconnell@hmc.edu>
2025-11-16 19:12:21 -0800

    updating I2S parameters and added debug clk divider

--- fpga/src/top.sv
+++ fpga/src/top.sv
@@ -1,14 +1,14 @@
-module top(//input  logic reset_n_i,      
-			//input  logic i2s_sd_i,         
-			//output logic lmmi_clk_i,    
-			//output logic i2s_sd_o,          
-			//output logic i2s_sck_o,        
-			//output logic i2s_ws_o,
+module top(input  logic reset_n_i,      
+			input  logic i2s_sd_i,         
+			output logic lmmi_clk_i,    
+			output logic i2s_sd_o,          
+			output logic i2s_sck_o,        
+			output logic i2s_ws_o,
 			output logic divided_clk);
 
-/*
+
 logic [5:0]  conf_res_i    = 6'd16;
-logic [9:0]  conf_ratio_i  = 10'd21;
+logic [9:0]  conf_ratio_i  = 10'd4;
 logic        conf_swap_i   = 1'b0;
 logic        conf_en_i     = 1'b1;
 
@@ -16,22 +16,21 @@ logic [31:0] adc_data;
 logic [31:0] dac_data;           
 logic        adc_valid;         
 logic        dac_request;       
-*/
+
 logic lmmi_clk_i;
-HSOSC hf_osc (
+HSOSC #(.CLKHF_DIV ("0b10")) hf_osc (
     .CLKHFPU(1'b1),
     .CLKHFEN(1'b1),
     .CLKHF(lmmi_clk_i));
 	
-//assign dac_data = adc_data;
+assign dac_data = adc_data;
 
 logic [23:0] counter = 0;
 always_ff @(posedge lmmi_clk_i)
 		counter <= counter + 1;
+assign divided_clk = counter[10];
 
-assign divided_clk = counter[10];
 
-/*
 lscc_i2s_codec #(
     .DATA_WIDTH       (16),
     .TRANSCEIVER_MODE (1)         
@@ -85,5 +84,5 @@ lscc_i2s_codec #(
 //output                     i2s_sd_o;      //-- I2S serial data output -- dout
 //output                     i2s_sck_o;     //-- I2S clock output --bclk
 //output                     i2s_ws_o;      //-- I2S word select output -- lrclk
-*/
+
 endmodule
