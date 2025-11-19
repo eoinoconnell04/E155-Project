6ee2902f4276c09579d30a4c71e502d1f68dad31
eoinoconnell04 <eoinoconnell04@gmail.com>
2025-11-18 23:44:39 -0800

    test unsigned

--- fpga/src/math_test.sv
+++ fpga/src/math_test.sv
@@ -1,13 +1,13 @@
-module math_test (input logic signed [3:0] tap,
-                  input logic signed [3:0] data,
-                  output logic signed [3:0] out);
+module math_test (input logic  [3:0] tap,
+                  input logic  [3:0] data,
+                  output logic  [3:0] out);
 
     // let tap = 1.125 in binary that would be 1.001.
     // shifting left by 3, *2^3 = 8, we get 1001 = 9
 
     // let data = 1011 = 11
 
-    logic signed [7:0] inter;
+    logic  [7:0] inter;
     assign inter = tap * data;
     assign out = inter >>> 3;
 
