f787a10994b328f7550e6fe2b05a7f56bc919956
eoinoconnell04 <eoinoconnell04@gmail.com>
2025-11-18 23:40:47 -0800

    fix syntax error

--- fpga/src/math_test.sv
+++ fpga/src/math_test.sv
@@ -1,13 +1,13 @@
-module math_test (input signed logic [3:0] tap,
-                  input signed logic [3:0] data,
-                  output signed logic [3:0] out);
+module math_test (input logic signed [3:0] tap,
+                  input logic signed [3:0] data,
+                  output logic signed [3:0] out);
 
     // let tap = 1.125 in binary that would be 1.001.
     // shifting left by 3, *2^3 = 8, we get 1001 = 9
 
     // let data = 1011 = 11
 
-    signed logic [7:0] inter;
+    logic signed [7:0] inter;
     assign inter = tap * data;
     assign out = inter >>> 3;
 
