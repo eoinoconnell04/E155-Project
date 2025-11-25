007999874496bdf9704b883c5f6b713eb4759281
eoinoconnell04 <eoinoconnell04@gmail.com>
2025-11-24 16:37:21 -0800

    compile test

--- fpga/src/eoin_testing/compile_test.sv
+++ fpga/src/eoin_testing/compile_test.sv
@@ -1,11 +1,5 @@
-module multaddsub_add_sign_7_6(a,b,c,din); 
-    parameter A_WIDTH = 3; 
-    parameter B_WIDTH = 3;
+module multaddsub_add_sign_7_6(input logic a,b,
+                                output logic c);
  
-    input signed [(A_WIDTH - 1):0] a; 
-    input signed [(B_WIDTH - 1):0] b;
-    input signed [(A_WIDTH + B_WIDTH - 1):0] din; 
-    output signed [(A_WIDTH + B_WIDTH - 1):0] c;
- 
-    assign c = a * b + din; 
+    assign c = a ^ b;
 endmodule
